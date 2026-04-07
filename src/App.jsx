5e551ff22386c5f9151b2b5faa900a1efb4a172a
Mahmoud Saied Elbath <mahmoud.elbath@gizasystems.com>
2026-04-07 22:35:34 +0200

    some changes as training add Parent and Child and loop object By Mahmoud Elbath

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import  Home from './Home/Home.jsx'
 import  About from './About/About.jsx'
+import Parent from './Parent/Parent.jsx'
 function App() {
   return (
     <div>
-      <Home />
+      <h1 className='text-center mt-5'>App Component</h1>
+      <Parent />
     </div>
   )
 }
